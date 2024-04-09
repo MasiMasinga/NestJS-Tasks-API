f6c602a4e164640a288b87dff5409d5cb63ccc7f
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-04-10 01:22:49 +0200

    feat - add validation pipes and exception handling to crud methods for controllers

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, IsOptional, IsBoolean } from 'class-validator';
+
 export class CreateTaskDto {
+    @IsNotEmpty()
+    @IsString()
     title: string;
+  
+    @IsOptional()
+    @IsBoolean()
     completed?: boolean;
-}
+  }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Param,
+    HttpException,
+    HttpStatus,
+    UsePipes,
+    ValidationPipe,
+    ParseIntPipe
+} from '@nestjs/common';
 
 // Task Service
 import { TasksService } from './tasks.service';
@@ -8,37 +21,75 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskResponseDto } from './dto/task-response.dto';
 
+
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Post()
+    @UsePipes(new ValidationPipe())
     async create(@Body() createTaskDto: CreateTaskDto) {
-        const result = await this.tasksService.create(createTaskDto);
-        return result;
+        try {
+            const result = await this.tasksService.create(createTaskDto);
+            return result;
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Internal Server Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     };
 
     @Get()
     async findAll(): Promise<TaskResponseDto[]> {
-        const tasks = await this.tasksService.findAll();
-        return tasks;
+        try {
+            const tasks = await this.tasksService.findAll();
+            return tasks;
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Internal Server Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        };
     };
 
     @Get(':id')
-    async findOne(@Param('id') id: number): Promise<TaskResponseDto> {
-        const task = await this.tasksService.findOne(id);
-        return task;
+    async findOne(@Param('id', ParseIntPipe) id: number): Promise<TaskResponseDto> {
+        try {
+            const task = await this.tasksService.findOne(id);
+            return task;
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Internal Server Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        };
     };
 
     @Put(':id')
+    @UsePipes(new ValidationPipe())
     async update(@Param('id') id: number, @Body() updateTaskDto: UpdateTaskDto) {
-        const result = await this.tasksService.update(id, updateTaskDto);
-        return result;
+        try {
+            const result = await this.tasksService.update(id, updateTaskDto);
+            return result;
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Internal Server Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     };
 
     @Delete(':id')
-    async remove(@Param('id') id: number) {
-        const result = await this.tasksService.remove(id);
-        return result;
+    async remove(@Param('id', ParseIntPipe) id: number) {
+        try {
+            const result = await this.tasksService.remove(id);
+            return result;
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Internal Server Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        };
     };
 }
